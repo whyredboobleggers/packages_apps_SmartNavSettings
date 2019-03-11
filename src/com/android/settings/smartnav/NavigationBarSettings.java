AospExtended/platform_packages_apps_SmartNavSettings

Add back navbar tuner
 * Add seperate preference for swipe up
 * Enable it only when stock mode selected
 * Add check for pie recents enabled
 * Deliberately disable swipe up when fling/smartbar selected

Signed-off-by: Shubham Singh <coolsks94@gmail.com>
Change-Id: I78c9e26c5d66cc8f7a4e715eaac737f5e6d51c6d

src/com/android/settings/smartnav/NavigationBarSettings.java
@@ -46,6 +46,7 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
     private static final String NAVBAR_VISIBILITY = "navbar_visibility";
     private static final String KEY_NAVBAR_MODE = "navbar_mode";
     private static final String KEY_DEFAULT_NAVBAR_SETTINGS = "default_settings";
+    private static final String KEY_SWIPE_GESTURES_SETTINGS = "swipeup_gesture_settings";
     private static final String KEY_FLING_NAVBAR_SETTINGS = "fling_settings";
     private static final String KEY_CATEGORY_NAVIGATION_INTERFACE = "category_navbar_interface";
     private static final String KEY_CATEGORY_NAVIGATION_GENERAL = "category_navbar_general";
@@ -63,6 +64,7 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
     private PreferenceCategory mNavGeneral;
     private PreferenceScreen mSmartbarSettings;
     private Preference mDefaultSettings;
+    private Preference mSwipeupSettings;
     private CustomSeekBarPreference mBarHeightPort;
     private CustomSeekBarPreference mBarHeightLand;
     private CustomSeekBarPreference mBarWidth;
@@ -78,6 +80,7 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
         mNavbarMode = (ListPreference) findPreference(KEY_NAVBAR_MODE);
         mDefaultSettings = (Preference) findPreference(KEY_DEFAULT_NAVBAR_SETTINGS);
+        mSwipeupSettings = (Preference) findPrefernce(KEY_SWIPE_GESTURES_SETTINGS);
         mFlingSettings = (PreferenceScreen) findPreference(KEY_FLING_NAVBAR_SETTINGS);
         mSmartbarSettings = (PreferenceScreen) findPreference(KEY_SMARTBAR_SETTINGS);
         mPulseSettings = (PreferenceScreen) findPreference(KEY_PULSE_SETTINGS);
@@ -124,6 +127,13 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             case 0:
                 mDefaultSettings.setEnabled(true);
                 mDefaultSettings.setSelectable(true);
+                if (isPieRecentsEnabled()) {
+                mSwipeupSettings.setEnabled(true);
+                mSwipeupSettings.setSelectable(true);
+                } else {
+                mSwipeupSettings.setEnabled(false);
+                mSwipeupSettings.setSelectable(false);
+                }
                 mSmartbarSettings.setEnabled(false);
                 mSmartbarSettings.setSelectable(false);
                 mFlingSettings.setEnabled(false);
@@ -132,6 +142,9 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             case 1:
                 mDefaultSettings.setEnabled(false);
                 mDefaultSettings.setSelectable(false);
+                disableSwipeup();
+                mSwipeupSettings.setEnabled(false);
+                mSwipeupSettings.setSelectable(false);
                 mSmartbarSettings.setEnabled(true);
                 mSmartbarSettings.setSelectable(true);
                 mFlingSettings.setEnabled(false);
@@ -140,12 +153,25 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             case 2:
                 mDefaultSettings.setEnabled(false);
                 mDefaultSettings.setSelectable(false);
+                disableSwipeup();
+                mSwipeupSettings.setEnabled(false);
+                mSwipeupSettings.setSelectable(false);
                 mSmartbarSettings.setEnabled(false);
                 mSmartbarSettings.setSelectable(false);
                 mFlingSettings.setEnabled(true);
                 mFlingSettings.setSelectable(true);
                 break;
         }
+    }
+
+    private boolean isPieRecentsEnabled() {
+       return Settings.System.getInt(getContentResolver(),
+                      Settings.System.RECENTS_COMPONENT, 0) == 0;
+    }
+
+    private void disableSwipeup() {
+       Settings.Secure.putInt(getContentResolver(),
+            Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
     }
 
     private void updateBarVisibleAndUpdatePrefs(boolean showing) {

whyredboobleggers/packages_apps_SmartNavSettings

src/com/android/settings/smartnav/NavigationBarSettings.java
@@ -46,6 +46,7 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
     private static final String NAVBAR_VISIBILITY = "navbar_visibility";
     private static final String KEY_NAVBAR_MODE = "navbar_mode";
     private static final String KEY_DEFAULT_NAVBAR_SETTINGS = "default_settings";
+    private static final String KEY_SWIPE_GESTURES_SETTINGS = "swipeup_gesture_settings";
     private static final String KEY_FLING_NAVBAR_SETTINGS = "fling_settings";
     private static final String KEY_CATEGORY_NAVIGATION_INTERFACE = "category_navbar_interface";
     private static final String KEY_CATEGORY_NAVIGATION_GENERAL = "category_navbar_general";
@@ -63,6 +64,7 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
     private PreferenceCategory mNavGeneral;
     private PreferenceScreen mSmartbarSettings;
     private Preference mDefaultSettings;
+    private Preference mSwipeupSettings;
     private CustomSeekBarPreference mBarHeightPort;
     private CustomSeekBarPreference mBarHeightLand;
     private CustomSeekBarPreference mBarWidth;
@@ -78,6 +80,7 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
         mNavbarMode = (ListPreference) findPreference(KEY_NAVBAR_MODE);
         mDefaultSettings = (Preference) findPreference(KEY_DEFAULT_NAVBAR_SETTINGS);
+        mSwipeupSettings = (Preference) findPreference(KEY_SWIPE_GESTURES_SETTINGS);
         mFlingSettings = (PreferenceScreen) findPreference(KEY_FLING_NAVBAR_SETTINGS);
         mSmartbarSettings = (PreferenceScreen) findPreference(KEY_SMARTBAR_SETTINGS);
         mPulseSettings = (PreferenceScreen) findPreference(KEY_PULSE_SETTINGS);
@@ -124,6 +127,13 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             case 0:
                 mDefaultSettings.setEnabled(true);
                 mDefaultSettings.setSelectable(true);
+                if (isPieRecentsEnabled()) {
+                mSwipeupSettings.setEnabled(true);
+                mSwipeupSettings.setSelectable(true);
+                } else {
+                mSwipeupSettings.setEnabled(false);
+                mSwipeupSettings.setSelectable(false);
+                }
                 mSmartbarSettings.setEnabled(false);
                 mSmartbarSettings.setSelectable(false);
                 mFlingSettings.setEnabled(false);
@@ -132,6 +142,9 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             case 1:
                 mDefaultSettings.setEnabled(false);
                 mDefaultSettings.setSelectable(false);
+                disableSwipeup();
+                mSwipeupSettings.setEnabled(false);
+                mSwipeupSettings.setSelectable(false);
                 mSmartbarSettings.setEnabled(true);
                 mSmartbarSettings.setSelectable(true);
                 mFlingSettings.setEnabled(false);
@@ -140,12 +153,25 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
             case 2:
                 mDefaultSettings.setEnabled(false);
                 mDefaultSettings.setSelectable(false);
+                disableSwipeup();
+                mSwipeupSettings.setEnabled(false);
+                mSwipeupSettings.setSelectable(false);
                 mSmartbarSettings.setEnabled(false);
                 mSmartbarSettings.setSelectable(false);
                 mFlingSettings.setEnabled(true);
                 mFlingSettings.setSelectable(true);
                 break;
         }
+    }
+
+    private boolean isPieRecentsEnabled() {
+       return Settings.System.getInt(getContentResolver(),
+                      Settings.System.RECENTS_LAYOUT_STYLE, 0) == 0;
+    }
+
+    private void disableSwipeup() {
+       Settings.Secure.putInt(getContentResolver(),
+            Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
     }
 
     private void updateBarVisibleAndUpdatePrefs(boolean showing) {